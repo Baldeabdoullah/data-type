9370d9df6c451ad08be7b52bed07fd781295b2a7
Aboullah Balde <abdoullahbalde@gmail.com>
2023-06-19 21:47:49 +0200

    commit du type de donne en js

---
+++ index.js
@@ -0,0 +1,52 @@
+// rappel des donnnees
+let string = "salut";
+let number = 23;
+let boolean = true;
+let maVariable;
+
+// les Tableaux
+let array = ["bordeaux", "toulouse", "Nantes"];
+// console.log(array[0][3]);
+
+let array2 = ["bordeaux", 24, true, [1, 2], { nom: "denis" }];
+
+// console.log(array2[4].nom);
+
+let objet = {
+  pseudo: "Denis",
+  age: 23,
+  techno: ["javascript", "react", "nodejs"],
+  admin: false,
+};
+
+// objet.adresse = "22 rue du code";
+// objet.age = 45;
+
+// console.log(objet);
+
+let data = [
+  {
+    pseudo: "Denis",
+    age: 23,
+    techno: ["javascript", "react", "nodejs"],
+    admin: false,
+  },
+
+  {
+    pseudo: "Samin",
+    age: 24,
+    techno: ["CSS", "react", "nodejs"],
+    admin: false,
+  },
+
+  {
+    pseudo: "Nikola",
+    age: 29,
+    techno: ["PHP", "react", "nodejs"],
+    admin: true,
+  },
+];
+
+// console.log(data[2].pseudo[0]);
+
+//
